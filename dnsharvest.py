f3e09480c2a0d27acb8f0f5b776a201b49a189c5
newfurniturey <newfurniturey@gmail.com>
2017-11-24 17:45:29 -0500

    Lookup each domain and print all matching IPs

--- dnsharvest.py
+++ dnsharvest.py
@@ -1,12 +1,22 @@
 #!/usr/bin/env python3
 
 import argparse;
+import socket;
 
 def processList(domain, filePath):
     with open(filePath) as fp:
         for line in fp:
             subdomain = line.strip();
-            print('%s.%s' % (subdomain, domain));
+            queryDomain('%s.%s' % (subdomain, domain));
+
+def queryDomain(domain):
+    try:
+        data = socket.gethostbyname(domain);
+        ip = repr(data);
+    except Exception:
+        return False;
+    
+    print("%s\t%s" % (domain, ip));
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Harvest the DNS');
